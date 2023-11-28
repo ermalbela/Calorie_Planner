5c8e11fc0140d24318a717875db3fa12d95e4125
ermalbela <ermal.bela1@gmail.com>
2023-11-29 00:09:17 +0100

    dark-only locaslstorage changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,26 @@
 const sun = document.querySelector('.sun');
 const moon = document.querySelector('.moon');
 
+if(localStorage.getItem('body-theme') == "light-only"){
+  document.body.className = 'light-only';
+  sun.style.display = 'none';
+  moon.style.display = 'block';
+} else{
+  document.body.className = 'dark-only';
+  moon.style.display = 'none';
+  sun.style.display = 'block';
+}
+
 sun.addEventListener('click', () => {
   sun.style.display = 'none';
   moon.style.display = 'block';
   document.body.className = 'light-only';
+  localStorage.setItem('body-theme', "light-only");
 })
 
 moon.addEventListener('click', () => {
   moon.style.display = 'none';
   sun.style.display = 'block';
   document.body.className = 'dark-only';
+  localStorage.setItem('body-theme', "dark-only");
 })
